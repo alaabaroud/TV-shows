2a19596e8cff68a4fedc2845fa8e2afdc7cae660
AlaaBaroud <>
2021-05-05 11:37:08 +0000

    trying js

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+alert('Hello There');
+
+var favoriteCharacter=prompt('do you like walter white or jesse pinkman more?')
+
+
+if (favoriteCharacter=='walter white'){
+document.write('<img src="https://memegenerator.net/img/instances/68218655.jpg"width: 200px>')
+}else{
+  document.write('<img src="https://wallpaperaccess.com/full/3115767.jpg" width: 200px>')
+}
+
+console.log('I made this with javascript');
