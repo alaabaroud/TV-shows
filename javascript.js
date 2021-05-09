8063c237c7dfb884086957e58c02690936fa2cf3
AlaaBaroud <>
2021-05-09 12:17:32 +0000

    practicing

--- javascript.js
+++ javascript.js
@@ -1,32 +1,16 @@
 
 //alert('Hello There');
 
-<<<<<<< HEAD
 function genre(){
 
-=======
->>>>>>> 2ab227e822bc52de31e9ad71a517769b10a667e2
 var genre=prompt('what do you like more comedy or drama?');
 while (genre !== 'comedy' && genre !== 'drama'){
 genre=prompt('choose one only, comedy or drama?');
 }
-<<<<<<< HEAD
+
 return genre;
 }
 genre()
-=======
-
-var rate=prompt('rate friends series on a scale of 1 to 10 ?');
-if (rate>10){
- rate=10;
-}
-for (var i = 1; i <= rate; i++) {
-  document.write ('<img src= "https://pyxis.nymag.com/v1/imgs/986/0a9/fb8617a064d7dee0e01b7335d9769a49ac-23-friends-cover-story-lede-mobile.rvertical.w570.jpg">')
-}
-
-
-var favoriteCharacter=prompt('do you like walter white or jesse pinkman more?')
-
 function rateFriends(){
 var rate=prompt('rate friends series on a scale of 1 to 10 ?');
 if (rate>10){
@@ -35,10 +19,8 @@ if (rate>10){
 for (var i = 1; i <= rate; i++) {
   document.write ('<img src= "https://pyxis.nymag.com/v1/imgs/986/0a9/fb8617a064d7dee0e01b7335d9769a49ac-23-friends-cover-story-lede-mobile.rvertical.w570.jpg">')
 }
-return rateFriends;
 }
-
-rateFriends()
+rateFriends();
 
 var character=function(){
 
@@ -48,9 +30,8 @@ if (favoriteCharacter=='walter white'){
 document.write('<img src="https://memegenerator.net/img/instances/68218655.jpg"width: 200px>')
 }else{
   document.write('<img src="https://wallpaperaccess.com/full/3115767.jpg" width: 200px>')
+} 
 }
-
 //character();
-/*
 console.log('I made this with javascript');
-*/
+
